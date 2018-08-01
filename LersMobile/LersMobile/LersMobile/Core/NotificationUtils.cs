0c22b8e31d599b0c1b621e2d31ae04924c1e4a83
gerasimov <gerasimov@lers.ru>
2018-08-01 15:55:39 +1000

    Переименование файла Core/NotificationHelper.cs на Core/NotificationUtils.cs (продолжение пред commit'а)

---
+++ LersMobile/LersMobile/LersMobile/Core/NotificationUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LersMobile.Core
+{
+	/// <summary>
+	/// Содержит вспомогательные методы.
+	/// </summary>
+	static class NotificationUtils
+	{
+		/// <summary>
+		/// Отображает в приложении информацию об уведомлении, открытую из "шторки"
+		/// </summary>
+		/// <param name="NotificationId">Идентификатор уведомления</param>
+		/// <returns></returns>
+		public static void ShowNotificationInfoPage(int NotificationId)
+		{
+			App.NotificationId = NotificationId;
+
+			var item = new MainPageMenuItem() { Title = Droid.Resources.Messages.MainPage_MenuItem_NotificationList, TargetType = typeof(NotificationCenterPage) };
+
+			((MainPage)Xamarin.Forms.Application.Current.MainPage).SwitchDetailToItem(item);
+		}
+	}
+}
