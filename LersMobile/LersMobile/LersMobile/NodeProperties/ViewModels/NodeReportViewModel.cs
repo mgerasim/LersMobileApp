238e880f534d923e1362a13c2f0338df34c4edab
gerasimov <gerasimov@lers.ru>
2018-08-07 16:58:15 +1000

    Добавлены параметры дата начало и дата окончания для формирования отчёта по точке учёта

--- LersMobile/LersMobile/LersMobile/NodeProperties/ViewModels/NodeReportViewModel.cs
+++ LersMobile/LersMobile/LersMobile/NodeProperties/ViewModels/NodeReportViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Lers.Core;
+﻿using Lers.Administration;
+using Lers.Core;
 using LersMobile.NodeProperties.ViewModels.Commands;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +13,14 @@ using Xamarin.Forms.Xaml;
 
 namespace LersMobile.NodeProperties.ViewModels
 {
-	public class NodeReportViewModel 
-	{
+	public class NodeReportViewModel : INotifyPropertyChanged
+    {
 		public NodeReportViewModel(NodeReport nodeReport)
 		{
             NodeReport = nodeReport;
             ReportCommand = new ReportCommand(this);
+            _dateBgn = DateTime.Now.AddDays(-7);
+            _dateEnd = DateTime.Now;
 		}
 
         public ReportCommand ReportCommand { get; set; }
@@ -31,8 +35,66 @@ namespace LersMobile.NodeProperties.ViewModels
             }
         }
         
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (propertyName != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private bool _isBusy;
+
+        public bool isBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged("isBusy");
+            }
+        }
+
+        private DateTime _dateBgn;
+
+        public DateTime dateBgn
+        {
+            get
+            {
+                return _dateBgn;
+            }
+            set
+            {
+                _dateBgn = value;
+                OnPropertyChanged("dateBgn");
+            }
+        }
+
+        private DateTime _dateEnd;
+
+        public DateTime dateEnd
+        {
+            get
+            {
+                return _dateEnd;
+            }
+            set
+            {
+                _dateEnd = value;
+                OnPropertyChanged("dateEnd");
+            }
+        }
+
         public void GenerateReport()
         {
+
+
+
             return;
         }
 
