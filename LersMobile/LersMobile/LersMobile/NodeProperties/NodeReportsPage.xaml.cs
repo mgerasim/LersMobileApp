fcd1a7061625e827f165f008ee3ffe7cc71b5551
gerasimov <gerasimov@lers.ru>
2018-08-07 14:16:38 +1000

    Добавлено свойства Выбранный отчет для класса страницы просмотра отчетов объекта учета

---
+++ LersMobile/LersMobile/LersMobile/NodeProperties/NodeReportsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using LersMobile.NodeProperties.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LersMobile.NodeProperties
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NodeReportsPage : ContentPage
+    {
+        NodeReportsViewModel ViewModel = null;
+
+        Core.NodeView NodeView = null;
+
+        public NodeReportsPage(Core.NodeView nodeView)
+        {
+            InitializeComponent();
+
+            this.Title = Droid.Resources.Messages.Text_Reports;
+
+            this.NodeView = nodeView;
+
+            ViewModel = new NodeReportsViewModel(this.NodeView.Node);
+            this.BindingContext = ViewModel;
+        }
+
+
+        /// <summary>
+        /// Вызывается при отображении страницы на экране.
+        /// </summary>
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await ViewModel.Load();
+        }
+    }
+}
---
+++ LersMobile/LersMobile/LersMobile/NodeProperties/ViewModels/NodeReportsViewModel.cs
@@ -0,0 +1,87 @@
+﻿using Lers.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LersMobile.NodeProperties.ViewModels
+{
+    public class NodeReportsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (propertyName != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public NodeReportsViewModel(Lers.Core.Node node)
+        {
+            Node = node;
+        }
+
+        private Lers.Core.Node Node = null;
+
+        public bool IsBusy { get; set; }
+                
+        public Lers.Core.NodeReportCollection Reports
+        {
+            get
+            {
+                return Node.Reports;
+            }
+        }
+
+        private NodeReport _selectedReport;
+
+        public NodeReport SelectedReport
+        {
+            get
+            {
+                return _selectedReport;
+            }
+            set
+            {
+                _selectedReport = value;
+                OnPropertyChanged("SelectedReport");
+            }
+        }
+
+        public async Task Load()
+        {
+            try
+            {
+                if (IsBusy)
+                {
+                    return;
+                }
+
+                IsBusy = true;
+
+                await App.Core.EnsureConnected();
+
+                var requiredFlags = NodeInfoFlags.Reports;
+
+                if (!Node.AvailableInfo.HasFlag(requiredFlags))
+                {
+                    await Node.RefreshAsync(requiredFlags);
+                    OnPropertyChanged("Reports");
+
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+    }
+}
