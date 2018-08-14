a1fb185f51d922b4b9ec919a6728916e918ea837
gerasimov <gerasimov@lers.ru>
2018-08-14 10:11:27 +1000

    добавлены иконки для отображения чекбокса. Добавлен тулбар для страницы объектов уучёта для генерации отчетов

--- LersMobile/LersMobile/LersMobile.Android/Resources/Resource.designer.cs
+++ LersMobile/LersMobile/LersMobile.Android/Resources/Resource.designer.cs
@@ -2342,26 +2342,26 @@ namespace LersMobile.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020151
-			public const int avd_hide_password_1 = 2130837841;
-			
-			// aapt resource value: 0x7f020152
-			public const int avd_hide_password_2 = 2130837842;
-			
 			// aapt resource value: 0x7f020153
-			public const int avd_hide_password_3 = 2130837843;
+			public const int avd_hide_password_1 = 2130837843;
+			
+			// aapt resource value: 0x7f020154
+			public const int avd_hide_password_2 = 2130837844;
+			
+			// aapt resource value: 0x7f020155
+			public const int avd_hide_password_3 = 2130837845;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020154
-			public const int avd_show_password_1 = 2130837844;
-			
-			// aapt resource value: 0x7f020155
-			public const int avd_show_password_2 = 2130837845;
-			
 			// aapt resource value: 0x7f020156
-			public const int avd_show_password_3 = 2130837846;
+			public const int avd_show_password_1 = 2130837846;
+			
+			// aapt resource value: 0x7f020157
+			public const int avd_show_password_2 = 2130837847;
+			
+			// aapt resource value: 0x7f020158
+			public const int avd_show_password_3 = 2130837848;
 			
 			// aapt resource value: 0x7f020055
 			public const int Check_16 = 2130837589;
@@ -3053,11 +3053,11 @@ namespace LersMobile.Droid
 			// aapt resource value: 0x7f02013a
 			public const int notification_icon_background = 2130837818;
 			
-			// aapt resource value: 0x7f02014f
-			public const int notification_template_icon_bg = 2130837839;
+			// aapt resource value: 0x7f020151
+			public const int notification_template_icon_bg = 2130837841;
 			
-			// aapt resource value: 0x7f020150
-			public const int notification_template_icon_low_bg = 2130837840;
+			// aapt resource value: 0x7f020152
+			public const int notification_template_icon_low_bg = 2130837842;
 			
 			// aapt resource value: 0x7f02013b
 			public const int notification_tile_bg = 2130837819;
@@ -3081,43 +3081,49 @@ namespace LersMobile.Droid
 			public const int report = 2130837825;
 			
 			// aapt resource value: 0x7f020142
-			public const int State_Error = 2130837826;
+			public const int select = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int State_Normal = 2130837827;
+			public const int State_Error = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int State_Unknown = 2130837828;
+			public const int State_Normal = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int State_Warning = 2130837829;
+			public const int State_Unknown = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int SystemType_ColdWater = 2130837830;
+			public const int State_Warning = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int SystemType_Electricity = 2130837831;
+			public const int SystemType_ColdWater = 2130837831;
 			
 			// aapt resource value: 0x7f020148
-			public const int SystemType_Gas = 2130837832;
+			public const int SystemType_Electricity = 2130837832;
 			
 			// aapt resource value: 0x7f020149
-			public const int SystemType_Heat = 2130837833;
+			public const int SystemType_Gas = 2130837833;
 			
 			// aapt resource value: 0x7f02014a
-			public const int SystemType_HotWater = 2130837834;
+			public const int SystemType_Heat = 2130837834;
 			
 			// aapt resource value: 0x7f02014b
-			public const int SystemType_Sewage = 2130837835;
+			public const int SystemType_HotWater = 2130837835;
 			
 			// aapt resource value: 0x7f02014c
-			public const int SystemType_Steam = 2130837836;
+			public const int SystemType_Sewage = 2130837836;
 			
 			// aapt resource value: 0x7f02014d
-			public const int tooltip_frame_dark = 2130837837;
+			public const int SystemType_Steam = 2130837837;
 			
 			// aapt resource value: 0x7f02014e
-			public const int tooltip_frame_light = 2130837838;
+			public const int tooltip_frame_dark = 2130837838;
+			
+			// aapt resource value: 0x7f02014f
+			public const int tooltip_frame_light = 2130837839;
+			
+			// aapt resource value: 0x7f020150
+			public const int unselect = 2130837840;
 			
 			static Drawable()
 			{
