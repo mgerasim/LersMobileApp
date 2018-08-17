18d980eacbe11b5e8ca0034525341adb5a71c0fe
gerasimov <gerasimov@lers.ru>
2018-08-17 16:38:15 +1000

    Исправлено:
    LRV-171
    
    Нужен комментарий с предназначением класса. Важный блок с переводом, нужно его хорошо документировать.
    
    http://app.lers.ru/cru/LRV-171#c509

--- LersMobile/LersMobile/LersMobile/TranslateExtension.cs
+++ LersMobile/LersMobile/LersMobile/TranslateExtension.cs
@@ -8,13 +8,34 @@ using Xamarin.Forms.Xaml;
 
 namespace LersMobile
 {
-    [ContentProperty("Text")]
+	/// <summary>
+	/// Предназначен для поддержки использования строковых констант в разметке xaml для свойства Text
+	/// </summary>
+	/// <remarks>
+	/// Требует подключение:
+	/// xmlns:local="clr-namespace:LersMobile"
+	/// Пример использования:
+	/// <Label Text="{local:Translate LoginPage_Server}" />
+	/// где LoginPage_Server - строковая константа, вынесенная в ресурс
+	/// </remarks>
+	[ContentProperty("Text")]
     public class TranslateExtension : IMarkupExtension
     {
+		/// <summary>
+		/// Расположение ресурса строковых констант
+		/// </summary>
         const string ResourceId = "LersMobile.Droid.Resources.Messages";
 
+		/// <summary>
+		/// Свойство элемента в разметке xaml для которого поддерживается применение строковых констант
+		/// </summary>
         public string Text { get; set; }
 
+		/// <summary>
+		/// Реализует получение строки по строковой константе из ресурса в зависимости от локализации 
+		/// </summary>
+		/// <param name="serviceProvider"></param>
+		/// <returns></returns>
         public object ProvideValue(IServiceProvider serviceProvider)
         {
             if (Text == null)
