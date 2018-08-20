f9f47a7a987a9ae31f07ca2e89f145c123ad12a9
gerasimov <gerasimov@lers.ru>
2018-08-20 15:41:41 +1000

    Исправления в терминалогии - строковая константа -> текстовый ресурс

--- LersMobile/LersMobile/LersMobile/TranslateExtension.cs
+++ LersMobile/LersMobile/LersMobile/TranslateExtension.cs
@@ -9,30 +9,30 @@ using Xamarin.Forms.Xaml;
 namespace LersMobile
 {
 	/// <summary>
-	/// Предназначен для поддержки использования строковых констант в разметке xaml для свойства Text
+	/// Предназначен для поддержки использования текстовых ресурсов в разметке xaml для свойства Text
 	/// </summary>
 	/// <remarks>
 	/// Требует подключение:
 	/// xmlns:local="clr-namespace:LersMobile"
 	/// Пример использования:
 	/// <Label Text="{local:Translate LoginPage_Server}" />
-	/// где LoginPage_Server - строковая константа, вынесенная в ресурс
+	/// где LoginPage_Server - текстовый ресурс
 	/// </remarks>
 	[ContentProperty("Text")]
     public class TranslateExtension : IMarkupExtension
     {
 		/// <summary>
-		/// Расположение ресурса строковых констант
+		/// Расположение ресурса текстовый ресурс
 		/// </summary>
         const string ResourceId = "LersMobile.Droid.Resources.Messages";
 
 		/// <summary>
-		/// Свойство элемента в разметке xaml для которого поддерживается применение строковых констант
+		/// Свойство элемента в разметке xaml для которого поддерживается применение текстовый ресурс
 		/// </summary>
         public string Text { get; set; }
 
 		/// <summary>
-		/// Реализует получение строки по строковой константе из ресурса в зависимости от локализации 
+		/// Реализует получение строки по текстовому ресурсу в зависимости от локализации 
 		/// </summary>
 		/// <param name="serviceProvider"></param>
 		/// <returns></returns>
